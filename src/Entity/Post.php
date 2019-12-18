5f0e13057cb29cfadd5272bf2a16a60f0e3ba885
Ilnar Kantuganov <Ilnar.Kantuganov@noveogroup.com>
2019-12-18 18:09:30 +0300

    displayed comments on post page

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\OrderBy;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PostRepository")
@@ -56,6 +57,7 @@ class Post
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="post")
+     * @OrderBy({"createdAt" = "DESC"})
      */
     private $comments;
 
