53e277061ad9a9d265349fcc125410765b63a766
Ilnar Kantuganov <Ilnar.Kantuganov@noveogroup.com>
2019-12-18 13:53:16 +0300

    changed post fixtures

--- src/DataFixtures/PostFixture.php
+++ src/DataFixtures/PostFixture.php
@@ -13,26 +13,35 @@ class PostFixture extends BaseFixture implements DependentFixtureInterface
 {
     protected function loadData(ObjectManager $manager): void
     {
-        $this->createMany(20, 'posts', function () {
-            $post = new Post();
-            $post->setTitle($this->faker->sentence);
-            $post->setBody($this->faker->realText());
-            $post->setCreatedAt(new \DateTime());
-            $post->setStatus(Post::STATUS_DRAFT);
+        $this->createMany(20, 'draft_posts', function () {
+            return $this->getPostByStatus(Post::STATUS_DRAFT);
+        });
 
-            /** @var User $author */
-            $author = $this->getRandomReference('confirmed_users');
-            $post->setAuthor($author);
-
-            $tags = $this->getRandomReferences('tags', $this->faker->numberBetween(0, 5));
-            foreach ($tags as $tag) {
-                $post->addTag($tag);
-            }
-
-            return $post;
+        $this->createMany(30, 'published_posts', function () {
+            return $this->getPostByStatus(Post::STATUS_PUBLISHED);
         });
 
         $manager->flush();
+    }
+
+    public function getPostByStatus(int $status)
+    {
+        $post = new Post();
+        $post->setTitle($this->faker->sentence);
+        $post->setBody($this->faker->realText());
+        $post->setCreatedAt($this->faker->dateTimeThisMonth);
+        $post->setStatus($status);
+
+        /** @var User $author */
+        $author = $this->getRandomReference('confirmed_users');
+        $post->setAuthor($author);
+
+        $tags = $this->getRandomReferences('tags', $this->faker->numberBetween(0, 5));
+        foreach ($tags as $tag) {
+            $post->addTag($tag);
+        }
+
+        return $post;
     }
 
     public function getDependencies()
