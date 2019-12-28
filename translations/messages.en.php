4ead31f7cb412537a2bb7fbfdb7ae7f819da6caf
Ilnar Kantuganov <Ilnar.Kantuganov@noveogroup.com>
2019-12-28 17:57:00 +0300

    added some translations and changed btn size

---
+++ translations/messages.en.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'Your Full Name' => 'Your Full Name',
+    'Email' => 'Email',
+    'Password' => 'Password',
+    'Retype Password' => 'Retype Password',
+    'I agree to the terms of service' => 'I agree to the terms of service',
+    'Title' => 'Title',
+    'Body' => 'Body',
+    'Preview' => 'Preview',
+    'Email could not be found.' => 'Email could not be found.',
+    'aaaa' => 'aaaa',
+];
---
+++ translations/validators.en.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'Please enter an email' => 'Please enter an email',
+    'Your email is too long' => 'Your email is too long',
+    'Please enter your full name' => 'Please enter your full name',
+    'Your full name is too long' => 'Your full name is too long',
+    'Please enter the title' => 'Please enter the title',
+    'The title is too long' => 'The title is too long',
+    'Please enter the preview' => 'Please enter the preview',
+    'The preview is too long' => 'The preview is too long',
+    'Please enter the body of a post' => 'Please enter the body of a post',
+];
